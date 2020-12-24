a906459ba123d705f262cc2daa31717ef4e6711b
Alejo Jubany <jubany4@gmail.com>
2020-12-23 22:26:36 -0300

    tratando d mostrar card, install router dom

--- memeFront/src/App.js
+++ memeFront/src/App.js
@@ -2,31 +2,26 @@ import React from 'react';
 import './App.css';
 import MyVerticallyCenteredModal from './Components/Categories/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap';
-<<<<<<< HEAD
+import { Button } from 'react-bootstrap';
 import Cards from './Components/Tarjetas/Cards';
-=======
 import Home from './Components/Home';
 import Item from './Components/Items';
 import Footer from './Components/Footer';
->>>>>>> f6a4a688dc21477132edcac266be08f1a5e1589c
+import NavBar from './Components/Navbar';
+
 
 
 function App() {
 
 
   return (
-  <>
-<<<<<<< HEAD
-  <NavBar/>
-  <Cards/>
-=======
-
- <Home/>
-<Item/>
-<Footer/>
->>>>>>> f6a4a688dc21477132edcac266be08f1a5e1589c
-  </>
+    <>
+      <NavBar />
+      <Cards />
+      <Home />
+      <Item />
+      <Footer />
+    </>
   );
 }
 
--- memeFront/src/Components/Tarjetas/Cards.js
+++ memeFront/src/Components/Tarjetas/Cards.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Card, Button, Container, Row,Col} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const Cards = ({memes, getmemes}) => {
-return(
-<Container>
-<Row>
-{memes.map((meme) => {
-<Col key={meme._id}>
-<Card style= {{width: "18rem"}}>
-<Link to={"/meme/"+ meme._id}>
-<Card.Img variant="top" src={getmemes.Link}/>
-<Card.Body>
-<Card.Title className="text-dark">{meme.itemName}</Card.Title>
-<Button variant="danger"> Ir al meme</Button>
-</Card.Body>
-</Link>
-</Card>
-</Col>
-})}
-</Row>
-</Container>
-);};
+const Cards = ({ items, getItems }) => {
+    return (
+        <Container>
+            <Row>
+                {items.map((item) => {
+                    <Col key={item._id}>
+                        <Card style={{ width: "18rem" }}>
+                            <Link to={"/meme/" + item._id}>
+                                <Card.Img variant="top" src={getItems(items.Link)} />
+                                <Card.Body>
+                                    <Card.Title className="text-dark">{item.itemName}</Card.Title>
+                                    <Button variant="danger"> Ir al meme</Button>
+                                </Card.Body>
+                            </Link>
+                        </Card>
+                    </Col>
+                }
+                )
+                }
+            </Row>
+        </Container>
+    );
+};
 
 export default Cards;
