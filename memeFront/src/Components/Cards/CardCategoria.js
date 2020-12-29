b5052a72a567439efaddd11988fdcaac358c4b89
Antonio Rotger <rotgerantonio@gmail.com>
2020-12-29 17:11:03 -0300

    Se arregla el mostrar memes por categorias en el Home

--- memeFront/src/Components/Cards/CardCategoria.js
+++ memeFront/src/Components/Cards/CardCategoria.js
@@ -1,49 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Cards from '../Tarjetas/Cards';
+import { Container } from "react-bootstrap";
+import Cards from "../Tarjetas/Cards";
 
-const CardCategoria = (props) => {
-    const [categoria, setCategoria] = useState([]);
-    const [meme, setMeme] = useState([]);
-    const [hasError, setErrors] = useState(false);
+const CardCategoria = () => {
+  const [categorias, setCategoria] = useState([]);
+  const [memes, setMemes] = useState([]);
+  const [hasError] = useState(false);
 
-    async function fetchData() {
-        const url = "https://proyecto-extra-rolling.herokuapp.com/api/categorys";
-        const res = await fetch(url);
-        console.log(res);
-        res
-            .json()
-            .then((res) => setCategoria(res))
-            .catch((err) => console.log("err" + err));
-            console.log(hasError);
-            console.log(categoria);
-    }
-    useEffect(() => {
-        fetchData();
-    }, []);
+  async function fetchData() {
+    const url = "https://proyecto-extra-rolling.herokuapp.com/api/categorys";
+    const res = await fetch(url);
+    res
+      .json()
+      .then((res) => {
+        setCategoria(res);
+      })
+      .catch((err) => console.log("err" + err));
+    const urlItems = "https://proyecto-extra-rolling.herokuapp.com/api/items/";
+    const resItems = await fetch(urlItems);
+    resItems
+      .json()
+      .then((res) => setMemes(res))
+      .catch((err) => console.log("err" + err));
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    
-
-    return (
-        <Container>
-            <div>
-               <h3>{meme.itemCategory}</h3> 
-                {categoria.map(item => (
-                   
-                        <ul key={item._id}>
-                             
-                        <h3>{item.name}</h3>  
-                        if ({item._id}=={item.itemCategory}) {
-                           <Cards />  
-                        }
-                        </ul>
-                ))
-                }               
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <div>
+        {categorias.map((categoria) => (
+          <ul key={categoria._id}>
+            <h3>{categoria.name}</h3>
+            {memes
+              .filter((x) => x.itemCategory == categoria._id)
+              .map((meme) => (
+                <li key={meme._id}>
+                  <h1>{meme.itemName}</h1>
+                  <img src = {meme.itemLink}></img>
+                </li>
+              ))}
+          </ul>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default CardCategoria;
--- memeFront/src/Components/Tarjetas/Cards.js
+++ memeFront/src/Components/Tarjetas/Cards.js
@@ -1,45 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 const Cards = (props) => {
-    const [memes, setMemes] = useState([]);
-    const [hasError, setErrors] = useState(false);
+  const [memes, setMemes] = useState([]);
+  const [hasError, setErrors] = useState(false);
 
-    async function fetchData() {
-        const url = "https://proyecto-extra-rolling.herokuapp.com/api/items/";
-        const res = await fetch(url);
-        console.log(res);
-        res
-            .json()
-            .then((res) => setMemes(res))
-            .catch((err) => console.log("err" + err));
-            console.log(hasError);
-            console.log(memes);
-    }
-    useEffect(() => {
-        fetchData();
-    }, []);
-    return (
-
-        
+  async function fetchData() {
+    const url = "https://proyecto-extra-rolling.herokuapp.com/api/items/";
+    const res = await fetch(url);
+    console.log(res);
+    res
+      .json()
+      .then((res) => setMemes(res))
+      .catch((err) => console.log("err" + err));
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
     <Container>
-            <div>
-                {memes.map(meme => (
-                   
-                        <ul key={meme._id}>
-                             
-                        <h3>{meme.itemName}</h3>   
-                        <h3>{meme.itemLink}</h3> 
-                        <h3>{meme.itemCommentary}</h3> 
-                        </ul>
-                ))
-                }               
-            </div>
-        
-       </Container>
-    );
+      <div>
+        {memes.map((meme) => (
+          <ul key={meme._id}>
+            <h3>{meme.itemName}</h3>
+            <img src={meme.itemLink}></img>
+            <h3>{meme.itemCommentary}</h3>
+          </ul>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Cards;
